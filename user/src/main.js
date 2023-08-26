c00349171971bae77a8548d5207ba9db0b65a94b
HaHoang <bhoangha.hp@gmail.com>
2023-08-26 19:40:38 +0700

    feat: pages user

--- user/src/main.js
+++ user/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import '@/axios'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
+
+// npm install vue-router
