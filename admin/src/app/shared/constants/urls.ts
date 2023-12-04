c30a8ce20b07679bb6c515862518ef380600186d
hahoang <bhoangha276@gmail.com>
2023-12-04 23:44:59 +0700

    fix: change urls api

--- admin/src/app/shared/constants/urls.ts
+++ admin/src/app/shared/constants/urls.ts
@@ -10,14 +10,19 @@ export const ACCOUNTS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/auth/filter`
 export const ACCOUNTS_SIGNUP_URL = `${BASE_URL}/${API_V1}/auth/signup`
 export const ACCOUNTS_LOGIN_URL = `${BASE_URL}/${API_V1}/auth/login`
 
-export const USERS_URL = `${BASE_URL}/${API_V1}/user`
-export const USERS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/user/filter`
+export const USERS_URL = `${BASE_URL}/${API_V1}/users`
+export const USERS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/users/filter`
 
-export const EMPLOYEES_URL = `${BASE_URL}/${API_V1}/employee`
-export const EMPLOYEES_BY_FILTER_URL = `${BASE_URL}/${API_V1}/employee/filter`
+export const EMPLOYEES_URL = `${BASE_URL}/${API_V1}/employees`
+export const EMPLOYEES_BY_FILTER_URL = `${BASE_URL}/${API_V1}/employees/filter`
 
-export const FOODS_URL = `${BASE_URL}/${API_V1}/product`
-export const FOODS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/product/filter`
+export const FOODS_URL = `${BASE_URL}/${API_V1}/products`
+export const FOODS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/products/filter`
 
-export const ORDERS_URL = `${BASE_URL}/${API_V1}/order`
-export const ORDERS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/order/filter`
+export const ORDERS_URL = `${BASE_URL}/${API_V1}/orders`
+export const ORDERS_BY_FILTER_URL = `${BASE_URL}/${API_V1}/orders/filter`
+
+// API VNPAY
+const VNPAY_URL = 'http://127.0.0.1:8888'
+
+export const PAYMENT_URL = `${VNPAY_URL}/create_payment_url`
--- user/src/app/shared/constants/urls.ts
+++ user/src/app/shared/constants/urls.ts
@@ -4,9 +4,9 @@ const LOCAL = 'http://127.0.0.1:8000/api/v1'
 
 const BASE_URL = OPENSHIFT || NETWORK || LOCAL
 
-export const FOODS_URL = `${BASE_URL}/product`
-export const FOODS_BY_ID_URL = `${BASE_URL}/product`
-export const FOODS_BY_NAME_URL = `${BASE_URL}/product/filter`
-export const FOODS_BY_TAG_URL = `${BASE_URL}/product/filter`
+export const FOODS_URL = `${BASE_URL}/products`
+export const FOODS_BY_ID_URL = `${BASE_URL}/products`
+export const FOODS_BY_NAME_URL = `${BASE_URL}/products/filter`
+export const FOODS_BY_TAG_URL = `${BASE_URL}/products/filter`
 
 export const ACCOUNT_LOGIN = `${BASE_URL}/auth/login`
